31e97f36db399d22ee58e3af77de76a136acbed5
BastianSD1 <139516022+BastianSD1@users.noreply.github.com>
2024-01-14 16:57:09 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function speak(){
+    
+    setTimeout(function() { 
+        take_snapshot(); 
+        save();
+    }, 5000);
+}
+function preoad(){
+    <Img>" src: https://i.postimg.cc/BbJqQ4DN/images.jpg    "</Img>
+
+}
+
+function setup(){
+    var cordenadas="x=200, y=200"  
+
+}
+function Draw(){
+    X:200 ;
+    Y: 200;
+
+}
